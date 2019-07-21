84b2b3bafd4909c4ba5ca22a383962f4210f2a69
mergitto <merugitosu@gmail.com>
2019-07-21 13:30:51 +0900

    zipファイルのリンク取得

--- scrape.rb
+++ scrape.rb
@@ -5,10 +5,12 @@ url = "https://www.acis.famic.go.jp/ddata/index2.htm"
 charset = "utf-8"
 
 doc = Nokogiri::HTML(open(url), nil, charset)
-doc.xpath('//a').each do |node|
-  next if node.attribute('href').nil?
-  next unless node.attribute('href').value.include?("zip")
-  p node.attribute('href').value
+doc.xpath('//div[@id="mainArea"]').each do |main_node|
+  main_node.xpath('.//a').each do |node|
+    next if node.attribute('href').nil?
+    next unless node.attribute('href').value.include?("zip")
+    p "#{node.inner_text} : #{node.attribute('href').value}"
+  end
 end
 
 
---
+++ setup.sh
@@ -0,0 +1,5 @@
+echo "start save csvfile"
+ruby open-write.rb
+
+echo "start scraping"
+ruby scrape.rb
