805926e80507efccdb1fc20043299e2100c1a589
mergitto <merugitosu@gmail.com>
2019-07-31 21:21:34 +0900

    カラム名で取得できるようにした

--- readcsv.rb
+++ readcsv.rb
@@ -1,11 +1,8 @@
 require 'csv'
-CSV.foreach("./tourokukihon.csv", encoding: "CP932:UTF-8") do |row|
+CSV.read("./tourokukihon.csv", headers: true, encoding: "CP932:UTF-8").each do |row|
   begin
-    p row
+    p row['農薬の種類']
   rescue => error
     puts error
   end
 end
-
-
-
